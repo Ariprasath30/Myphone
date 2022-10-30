7d5c761523172b91eb6c282d200ae39f0f645f4a
Ariprasath30 <prasathari30@gamil.com>
2022-10-30 17:09:24 +0530

    dlt task

--- src/org/phone/PhoneInfo.java
+++ src/org/phone/PhoneInfo.java
@@ -3,7 +3,7 @@ package org.phone;
 public class PhoneInfo {
 	public void phoneName()
 	{
-		System .out.println("phone name;poco");
+		System .out.println("phone name;pocof4");
 	}
 	public void phoneMienum()
 	{
@@ -17,10 +17,7 @@ public class PhoneInfo {
 	{
 		System .out.println("Storage;64gb");
 	}
-	public void osName()
-	{
-		System .out.println("osName;anroid 11");
-	}
+	
 	//main method creation
 	//main ctrl+space and press enter
 	public static void main(String[]args) {
@@ -33,7 +30,6 @@ public class PhoneInfo {
 		p.phoneMienum();
 		p.Camera();
 		p.Storage();
-		p.osName();
 				
 		
 	}
